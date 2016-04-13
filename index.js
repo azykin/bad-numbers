93d1dc9884c89c541a053ea1b85e75ce73eb88e3
Nikita Lebedev <goodcat.abc@gmail.com>
2016-04-14 01:56:48 +0300

    Added simple html and angular files

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var port = process.env.port || 3000;
+var dbPath = __dirname + '/db';
+
+app.use(express.static(__dirname + '/public'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/angular', express.static(__dirname + '/node_modules/angular'))
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile('./public/index.js');
 });
 
-app.listen(3000, function () {
+app.get('/phones', function (req, res) {
+    res.sendFile(dbPath + '/phones.json');
+});
+
+app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
