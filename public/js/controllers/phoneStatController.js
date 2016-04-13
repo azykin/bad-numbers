32e264b90fb5158784ab28a40125e64cd5451edd
Nikita Lebedev <goodcat.abc@gmail.com>
2016-04-14 02:04:25 +0300

    Added data, app and controllers

---
+++ public/js/controllers/phoneStatController.js
@@ -0,0 +1,7 @@
+﻿angular.module('app')
+    .controller('phoneStatController', function ($scope, $http) {
+    $http.get('/phones')
+    .then(function (phones) {
+        $scope.phones = phones.data;
+    });
+});
